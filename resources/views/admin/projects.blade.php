6633453e5d683f8fc8310b82a66f9789cb242d89
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-06-19 18:48:10 +0200

    feat: index CRUD function and projects.blade.php

---
+++ resources/views/admin/projects.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead class="thead-dark">
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($projects as $project)
+                    <tr>
+                        <td>{{ $project->id }}</td>
+                        <td>{{ $project->name }}</td>
+                        <td>{{ $project->description }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
