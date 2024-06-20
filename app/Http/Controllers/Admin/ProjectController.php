958b7da6b14e410cfe51be8d84e9549e7db9e078
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-06-20 10:29:59 +0200

    feat: create and store CRUD operations and fix to index, also new create page

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
 
 class ProjectController extends Controller
 {
@@ -15,7 +17,7 @@ class ProjectController extends Controller
     {
         $projects = Project::all();
 
-        return view('admin.projects', compact('projects'));
+        return view('admin.projects.index', compact('projects'));
     }
 
     /**
@@ -23,7 +25,7 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.projects.create');
     }
 
     /**
@@ -31,7 +33,14 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+        $newProject = new Project();
+        $newProject->name = $data['name'];
+        $newProject->description = $data['description'];
+        $newProject->slug = Str::slug($newProject->name);
+        $newProject->save();
+
+        return redirect()->route('admin.projects.index');
     }
 
     /**
